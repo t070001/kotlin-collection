3f76bf44f39d9935347c5f84194cc15186128b47
Fun Z <t070001@gmail.com>
2023-01-22 00:33:05 +0800

    Update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,5 @@
 fun main(args: Array<String>) {
-    println("key in name :")
-    var my = readLine()
-    println("Hello! "+my)
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println("What's your name?")
+    val name= readLine()
+    println("Hello $name!")
 }
