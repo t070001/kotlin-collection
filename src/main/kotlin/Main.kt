6461cec12a85c80c1b391928fe4be3d2e6b08583
Fun Z <t070001@gmail.com>
2023-01-22 00:29:38 +0800

    Commit Message

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    println("key in name :")
+    var my = readLine()
+    println("Hello! "+my)
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
